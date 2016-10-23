6b8d664afdde9fdee8ab10c0fee289199b5314a3
Vipul Munot <vipmunot@gmail.com>
2016-10-23 18:52:00 -0400

    new decision tree implementation

--- ML/Python/Decision Tree/decision tree.py
+++ ML/Python/Decision Tree/decision tree.py
@@ -1,202 +1,127 @@
-import math
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 18 10:38:03 2016
 
-class Node(object):
-    def __init__(self,columnIndex,columnName):
-        self.columnIndex = columnIndex
-        self.columnName = columnName
-        self.children = {}
+@author: Vipul Munot
+"""
+tennis=[line.strip().split(',') for line in open('tennis.txt')]
+test_tennis=[line.strip().split(',') for line in open('test-tennis.txt')]
+data=[['slashdot','USA','yes',18,'None'],
+        ['google','France','yes',23,'Premium'],
+        ['digg','USA','yes',24,'Basic'],
+        ['kiwitobes','France','yes',23,'Basic'],
+        ['google','UK','no',21,'Premium'],
+        ['(direct)','New Zealand','no',12,'None'],
+        ['(direct)','UK','no',21,'Basic'],
+        ['google','USA','no',24,'Premium'],
+        ['slashdot','France','yes',19,'None'],
+        ['digg','USA','no',18,'None'],
+        ['google','UK','no',18,'None'],
+        ['kiwitobes','UK','no',19,'None'],
+        ['digg','New Zealand','yes',12,'Basic'],
+        ['slashdot','UK','no',21,'None'],
+        ['google','UK','yes',18,'Basic'],
+        ['kiwitobes','France','yes',19,'Basic']]
 
+class node:
+    def __init__(self,col=-1,value=None,results=None,left=None,right=None):
+        self.col=col
+        self.value = value
+        self.results=results # Stores Predicted Output (None: for non leaf nodes)
+        self.left=left # True values
+        self.right=right # False values
 
-    def __repr__(self):
-        result = "[Node:" + self.columnName + "], child = {"
-        for name, value in self.children.items():
-            result += name+ "=" + repr(value) + ", "
-        return result + "}]"
+def split_data(data,column,value):
+    split = None
+    if isinstance(value,int) or isinstance(value,float):
+        split = lambda item:item[column]>=value
+    else:
+        split = lambda item:item[column]==value
+    subset1 = [item for item in data if split(item)]
+    subset2 = [item for item in data if not split(item)]
+    return (subset1,subset2)
 
-def cal_frequency(feature):
-    freq = {}
-    for val in feature:
-        if val not in freq:
-            freq[val] = 0
-        freq[val] +=1
-    return freq
+r1,r2 = split_data(data,2,'yes')
+#print ("\nSubset 1: ",r1)    
+#print ("\nSubset 2: ",r2)    
 
-def cal_entropy(output):
-    freq = cal_frequency(output)
-    result = 0.0
-    for val in freq.values():
-        prob = float(val)/len(output)
-        result -= prob * math.log(prob,2)
+def frequency(data):
+    result ={}
+    for value in data:
+        cols = value[len(value)-1]
+        if cols not in result: result[cols]=0
+        result[cols]+=1
     return result
-
-def filter_data(feature,output,index,value):
-    new_features = []
-    new_output = []
-    for i in range(len(feature)):
-        if feature[i][index] == value:
-            new_features.append(feature[i])
-            new_output.append(output[i])
-    return new_features, new_output
-
-def get_columns(features,index):
-    result = []
-    for row in features:
-        result.append(row[index])
+    
+def entropy(data):
+    result =0.0
+    freq = frequency(data)
+    for val in freq.keys():
+        import math
+        prob = float(freq[val])/len(data)
+        result = result - prob*math.log2(prob)
     return result
+#print ("\nEntropy: ",entropy(data))
+#print ("\nEntropy: ",entropy(r1))    
 
-def drop_column(features,index):
-    result = []
-    for row in features:
-        new_row = []
-        for i in range(len(row)):
-            if i !=index:
-                new_row.append(row[i])
-        result.append(new_row)
-    return result
+def build_tree(rows):
+  if len(rows)==0: return node()
+  currentScore=entropy(rows)
 
-def information_gain(features,output,index):
-    column = get_columns(features,index)
-    freq = cal_frequency(column)
-    unique_val = set(column)
-    result = 0.0
-    for value in unique_val:
-        new_features,new_output = filter_data(features,output,index,value)
-        new_features = drop_column(new_features,index)
-        entropy = cal_entropy(new_output)
-        pi = freq[value] / float(len(output))
-        result += pi * entropy
-    return cal_entropy(output) - result
+  best_gain=0.0
+  best_criteria=None
+  best_sets=None
+  
+  column_count=len(rows[0])-1
+  for col in range(0,column_count):
 
-def best_attribute(features,output,columns):
-    maxInfoGain = -1.0
-    best_feature = -1
-    best_feature_index = -1
-    for i in range(len(features[0])):
-        gain = information_gain(features,output,i)
-        if gain>maxInfoGain:
-            maxInfoGain=gain
-            best_feature_index=i
-            best_feature = columns[i]
-    return best_feature_index,best_feature
+    column_values={}
+    for row in rows:
+       column_values[row[col]]=1
+    for value in column_values.keys():
+      (set1,set2)=split_data(rows,col,value)
 
-import operator
-def max_prob(feature):
+      p=float(len(set1))/len(rows)
+      gain=currentScore-p*entropy(set1)-(1-p)*entropy(set2)
+      if gain>best_gain and len(set1)>0 and len(set2)>0:
+        best_gain=gain
+        best_criteria=(col,value)
+        best_sets=(set1,set2)
+  
+  if best_gain>0:
+    trueBranch=build_tree(best_sets[0])
+    falseBranch=build_tree(best_sets[1])
+    return node(col=best_criteria[0],value=best_criteria[1],
+                        left=trueBranch,right=falseBranch)
+  else:
+    return node(results=frequency(rows))
+tree = build_tree(data) 
+tennis_tree = build_tree(tennis) 
+def print_tree(tree,indent = ' '):
+    if tree.results!= None:
+        print (str(tree.results))
+    else:
+        print (str(tree.col)+':'+str(tree.value))
+        print (indent+ 'Left ->',print_tree(tree.left,indent+' '))
+        print (indent+ 'Right ->',print_tree(tree.right,indent+' '))
 
-    result = {}
-    for item in feature:
-        if item not in result.keys():
-            result[item] = 0
-        result[item] +=1
-    sortedClassCount=sorted(result.iteritems(),key=operator.itemgetter(1),reverse=True)
-    return sortedClassCount
+print (print_tree(tree))        
+print ("\nTennis:\n")
+print (print_tree(tennis_tree))        
 
-def new_columns(columns,attr):
-    result = []
-    result = columns[:attr]
-    result.extend(columns[attr+1:])
-    return result
+def classify(observation,tree):
+    if tree.results!=None: return tree.results
+    else:
+        val = observation[tree.col]
+        branch = None
+        if isinstance(val,int)or isinstance(val,float):
+            if val >=tree.value: branch= tree.left
+            else: branch = tree.right
+        else:
+            if val == tree.value: branch=tree.left
+            else: branch = tree.right
+    return classify(observation,branch)
 
-def build_tree(features,output,columns):
-    if output.count(output[0])==len(output):
-        return output[0]
-    if len(features[0])==1:
-        return max_prob(output)
-    attr,attr_name = best_attribute(features,output,columns)
-    root = Node(attr,attr_name)
-    for value in set(get_columns(features,attr)):
-        new_features, new_output = filter_data(features, output, attr, value)
-        new_cols = new_columns(columns,attr)
-        new_features = drop_column(new_features,attr)
-        root.children[value] = build_tree(new_features,new_output,new_cols)
-    return root
-
-def grow_tree(features, output, columns):
-    if output.count(output[0]) == len(output):
-        return output[0]
-    if len(features[0])==1:
-        return max_prob(output)
-    attr, attr_name = best_attribute(features, output, columns)
-    attr_output = columns[attr]
-
-    DecisionTree = {attr_output: {}}
-    del (columns[attr])
-    featureValues = [item[attr] for item in features]
-
-    unique_value = set(featureValues)
-    for value in unique_value:
-        new_features, new_output = filter_data(features, output, attr, value)
-        new_features = drop_column(new_features, attr)
-
-        DecisionTree[attr_output][value] = build_tree(new_features, new_output, columns)
-    return DecisionTree
-
-
-def predict_class(Dtree,features,columns):
-
-    for i in range((len(columns)-1)):
-        if columns[i]== Dtree.columnName:
-            for name, value in Dtree.children.items():
-                if features[i]== name:
-                    if  hasattr(Dtree,'children'):
-                        new_features = new_columns(features,i)
-                        new_cols = new_columns(columns,i)
-                        #print tree.children['Sunny'].children.values()
-                        return predict_class(value,new_features,new_cols)
-                    else:
-                        return value
-               # print "child:",name, value, features[i]
-        #print columns[i]
-    #print tree.columnName
-    #print tree.children
-
-def children(value):
-    for name, item in value:
-        return name, item
-'''
-def predict_class(tree,features,columns):
-    for i in range((len(columns)-1)):
-        if columns[i]== tree.columnName:
-            continue
-        for name, value in tree.children.iteritems():
-            if features[i] == name:
-                #new_features = new_columns(features, i)
-                #new_cols = new_columns(columns, i)
-                print "child:", name, value, features[i]
-                i +=1
-'''
-
-def main():
-    data=[line.strip().split(',') for line in open('tennis.txt')]
-    features = [item[:-1] for item in data]
-    output = [item[-1] for item in data]
-    columns = ['Outlook', 'Temperature', 'Humidity', 'Windy', 'PlayTennis']
-    monk = [line.strip().split(' ') for line in open('monks-1.csv')]
-    monk_features = [item[1:-1] for item in monk]
-    monk_output = [item[0] for item in monk]
-    monk_columns = ["a1","a2","a3","a4","a5","a6"]
-   # print cal_entropy(output)
-    #print filter_data(features,output,0,'Rain')
-
-    #print information_gain(features,output,0)
-    #print get_columns(features,0)
-    #print drop_column(features, 2)
-
-    #print best_attribute(features,output,columns)
-    print build_tree(features,output,columns)
-    #print cal_entropy(monk_output)
-    # print filter_data(features,output,0,'Rain')
-    # print information_gain(features,output,0)
-    # print get_columns(features,0)
-    # print drop_column(features, 2)
-    tree = build_tree(features,output,columns)
-    predict_data = [line.strip().split(',') for line in open('test-tennis.txt')]
-    predict = [item for item in predict_data]
-    for i in range(len(predict)):
-        print predict[i]
-        print predict_class(tree,predict[i],columns)
-    #print predict_class(tree,predict,columns)
-    #print build_tree(monk_features,monk_output,monk_columns)
-   # print "using Dictionary: ",grow_tree(monk_features,monk_output,monk_columns)
-    #print "using Dictionary: ",grow_tree(features, output, columns)
-
-main()
+print ("\nClasificaton: ",classify(['(direct)','USA','yes',5],tree))    
+for values in test_tennis:
+    print ("\n",values,"\tClasificaton: ",classify(values,tennis_tree))
